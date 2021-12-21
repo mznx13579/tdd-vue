8e4f4793b45828d4029bc175cde2af48905d3c41
Henry <jeongsy@gabia.com>
2021-12-21 22:13:58 +0900

    fix: 기본 flex 가운데 정렬로 수정

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,10 @@ export default Vue.extend({
 
 <style lang="scss" scope>
 .app {
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
   background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
   background-size: 400% 400%;
   animation: gradient 15s ease infinite;
