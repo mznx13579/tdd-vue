957b36e57818b5ebadb4204497fe597820d8a245
Henry <jeongsy@gabia.com>
2021-12-21 22:14:25 +0900

    add: Welcome Page 추가

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <div class="welcome">
+      <p class="welcome__title">루팡 계산기</p>
+      <p class="welcome__desc">재미로 하는 거에요!</p>
+      <button class="welcome__button" @click="goToNext">Start</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,14 +13,42 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Home',
+  methods: {
+    goToNext() {
+      this.$router.push('/money');
+    },
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 .home {
   display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+  .welcome {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &__title {
+      font-size: 3rem;
+    }
+
+    &__desc {
+      font-size: 1.5rem;
+      padding: 20px 0;
+    }
+
+    &__button {
+      height: 50px;
+      width: 50%;
+      font-size: 1.2rem;
+      color: #efefef;
+      background-color: rgba($color: #fff, $alpha: 0.2);
+      border-radius: 10px;
+      box-shadow: 5px 5px 5px #f9f871;
+    }
+    &__button:hover {
+    }
+  }
 }
 </style>
