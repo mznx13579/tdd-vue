6a0b269d9b4cd07496b7bbe892a524cbfe921214
Henry <jeongsy@gabia.com>
2021-12-27 20:58:49 +0900

    add: 연봉을 입력하고 다음 버튼을 눌렀을 때 tdd 개발

--- cypress/integration/0-Money/InputMoney.spec.ts
+++ cypress/integration/0-Money/InputMoney.spec.ts
@@ -5,6 +5,30 @@ describe('Input Money', () => {
 
   it('연봉을 입력하지 않고 다음 버튼을 누르면 알림 메세지가 표시된다.', () => {
     cy.get('.next-button').click();
-    cy.get('.warning-message').should('have.text', '연봉을 입력해야 해요!');
+    cy.get('.warning-message').should('have.text', '!연봉을 입력해야 해요!');
+  });
+
+  context('연봉을 입력하고 다음 버튼을 누르면', () => {
+    beforeEach(() => {
+      cy.get('.money__wrap--input').clear().type('4300');
+      cy.get('.next-button').click();
+    });
+
+    it('input이 없어져야 한다.', () => {
+      cy.get('input.money__wrap--input').should('not.exist');
+    });
+
+    it('연봉+만원 string이 노출 되야 한다.', () => {
+      cy.get('.money__wrap').children().should('have.text', '4300만원');
+    });
+
+    it(`버튼이 '확인'으로 바뀌어야 한다.`, () => {
+      cy.get('.next-button').should('have.text', '확인!');
+      cy.get('.next-button').should('not.have.text', '다음!');
+    });
+
+    it(`'확인!' 버튼을 누르면 다음 페이지로 이동한다.`, () => {
+      cy.contains('확인!').click();
+    });
   });
 });
--- src/pages/Money.vue
+++ src/pages/Money.vue
@@ -1,16 +1,48 @@
 <template>
   <div class="money">
-    <p class="money__title">연봉을 입력해 주세요!</p>
-    <input />
-    <p class="warning-message">연봉을 입력해야 해요!</p>
-    <button class="next-button">다음!</button>
+    <p class="money__title" :class="{ 'warning-message': showAlert }">{{ moneyTitleDesc }}</p>
+    <div v-if="!showDesc" class="money__wrap">
+      <input v-model.number="money" class="money__wrap--input" />
+      <span class="money__wrap--unit">만원</span>
+    </div>
+    <div v-else class="money__wrap">
+      <p class="money__wrap--unit">{{ `${money}만원` }}</p>
+    </div>
+    <button v-if="!showDesc" class="next-button" @click="goNext">다음!</button>
+    <button v-else class="next-button" @click="confirm">확인!</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  name: 'Money',
+  data() {
+    return {
+      money: 0 as number,
+      showAlert: false as boolean,
+      moneyTitleDesc: '연봉을 입력해 주세요!' as string,
+      showDesc: false as boolean,
+    };
+  },
+
+  methods: {
+    goNext() {
+      if (this.money === 0) {
+        this.showAlert = true;
+        this.moneyTitleDesc = '!연봉을 입력해야 해요!';
+        return;
+      }
+      this.showDesc = true;
+      this.moneyTitleDesc = '연봉은!';
+    },
+
+    confirm() {
+      console.log('comfirm');
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped>
@@ -20,5 +52,22 @@ export default Vue.extend({});
   font-weight: 700;
   display: flex;
   flex-direction: column;
+  align-items: center;
+
+  &__title.warning-message {
+    color: #cdfb86;
+  }
+  &__wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &--input {
+      color: #676767;
+      width: 300px;
+    }
+    &--unit {
+      font-size: 3rem;
+    }
+  }
 }
 </style>
