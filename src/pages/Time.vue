d3a942e23e67c3a48d6dbd0030e612d7a8f961d7
Henry <jeongsy@gabia.com>
2021-12-28 17:45:20 +0900

    add: 시간 계산식 추가

--- src/pages/Time.vue
+++ src/pages/Time.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="time">
-    <div class="food-list">
-      
-    </div>
+    <div class="food-list"></div>
   </div>
 </template>
 
@@ -12,12 +10,16 @@ import Vue from 'vue';
 interface Food {
   name: string;
 }
+
+/**
+ * 계산식 : 연봉(만원단위 x) / 260일 * 10000 / 32400초
+ * 1일에 9시간 1년에 260일
+ */
 export default Vue.extend({
   name: 'Time',
   data() {
     return {
       money: 0 as number,
-      foodList:
     };
   },
   created() {
